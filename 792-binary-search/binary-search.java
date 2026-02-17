adca479c7f05c5731e5ea8f0b22acd2dece118f6
alibahi0001 <viramaraharv@gmail.com>
2026-02-17 22:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (96.54%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return fastsearch(nums, target, 0, nums.length - 1);
+    }
+
+    public int fastsearch(int[] nums, int target, int left, int right) {
+        
+        if (left > right)   // base case
+            return -1;
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (target > nums[mid]) {
+            return fastsearch(nums, target, mid + 1, right);
+        } else {
+            return fastsearch(nums, target, left, mid - 1);
+        }
+    }
+}
